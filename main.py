3c53f86cbbe41f49880da97af7098b5a2e5c5114
dungdang08 <danghoangdung08@gmail.com>
2023-10-03 23:44:46 +0200

    Try with git lfs

--- main.py
+++ main.py
@@ -23,47 +23,42 @@ st.write("""
 Using CNN to classify whether a skin mole is benign or malignant
 """)
 
-st.sidebar.subheader("CNN model parameters")
-st.sidebar.text("""
+st.sidebar.write("# CNN models")
+st.sidebar.write("""
+## Result 1:
 
-Hyperparameters:
+- Used model: Pretrained VGG16
+- Optimiser: SGD with scheduler
+- Loss Function: Cross Entropy Loss
 - Batch size: 16
 - Learning rate: 0.001
-- Number of epochs: 20 
-- Number of classes: 2
-- Number of channels: 3
+- Number of epochs: 20
+(Early stopping)
+- Test accuracy: ~89%
 
-Preprocessing images:
-- Resize 256x256
-- Center crop 224x224 
-- Normalization
+## Result 2:
 
-Optimizer:
-- Custom CNN model: Adam
-- VGG 16: SGD with scheduler
+- Used model: Pretrained AlexNet
+- Optimiser: SGD with scheduler
+- Loss Function: Cross Entropy Loss
+- Batch size: 30
+- Learning rate: 0.001
+- Number of epochs: 25
+(Early stopping)
+- Test accuracy: ~97%
 
-Loss Function:
-- Cross Entropy Loss
 """)
 
-uploaded = st.file_uploader(label = "", type=".jpg", accept_multiple_files=False)
-#for image in uploaded:
+uploaded = st.file_uploader(label="", type=".jpg", accept_multiple_files=False)
+# for image in uploaded:
 #    image1 = Image.open(image)
 #    print(type(image1))
 
-def predict(image):
-    """Return top 5 predictions ranked by highest probability.
 
-    Parameters
-    ----------
-    :param image: uploaded image
-    :type image: jpg
-    :rtype: list
-    :return: top 5 predictions ranked by highest probability
-    """
+def predict1(image):
     # load pretrained vgg16 model
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    vgg16 = models.vgg16_bn(pretrained=False)
+    vgg16 = models.vgg16_bn(weights=None)
 
     # Freeze training for all layers
     for param in vgg16.features.parameters():
@@ -75,7 +70,7 @@ def predict(image):
     features.extend([nn.Linear(num_features, 2)]) # Add our layer with 2 outputs
     vgg16.classifier = nn.Sequential(*features) # Replace the model classifier
 
-    #Load pretrained weights
+    # Load pretrained weights
     vgg16.load_state_dict(torch.load("model_weights.pth", map_location=torch.device('cpu')))
     vgg16.to(device)
 
@@ -99,21 +94,64 @@ def predict(image):
     return predicted.item()
 
 
+def predict2(image):
+    # load pretrained vgg16 model
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    alexnet = torch.hub.load('pytorch/vision:v0.10.0', 'alexnet', weights=None)
+
+    # Freeze training for all layers
+    for param in alexnet.features.parameters():
+        param.require_grad = False
+
+    # Newly created modules have require_grad=True by default
+    num_features = alexnet.classifier[6].in_features
+    features = list(alexnet.classifier.children())[:-1]  # Remove last layer
+    features.extend([nn.Linear(num_features, 2)])  # Add our layer with 2 outputs
+    alexnet.classifier = nn.Sequential(*features)  # Replace the model classifier
+
+    #  pretrained weights
+    alexnet.load_state_dict(torch.load("model_weights2.pth", map_location=torch.device('cpu')))
+    alexnet.to(device)
+
+    # transform the input image through resizing, normalization
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )])
+
+    # Load the image, pre-process it, and make predictions
+    img = Image.open(image)
+    img = transform(img).unsqueeze(0)
+    img = Variable(img)
+    alexnet.eval()
+    out = alexnet(img)
+    _, predicted = torch.max(out, 1)
+    return predicted.item()
+
+
 if uploaded is not None:
     # display image that user uploaded
     image = Image.open(uploaded)
     st.image(image, caption = 'Uploaded Image', use_column_width = True)
     st.write("")
-    st.write("Making prediction ...")
-    label = predict(uploaded)
-    if label == 1:
-        result = "MALIGNANT"
+    st.write("Making predictions ...")
+
+    label1 = predict1(uploaded)
+    if label1 == 1:
+        result1 = "MALIGNANT"
     else:
-        result = "BENIGN"
-    st.subheader("Result: ")
-    st.write(result)
+        result1 = "BENIGN"
+    st.subheader("Result 1 (VGG16): ")
+    st.write(result1)
 
-##==================================LOAD CNN MODEL=====================================================
-
-
-##======================================================================================================
+    label2 = predict2(uploaded)
+    if label2 == 1:
+        result2 = "MALIGNANT"
+    else:
+        result2 = "BENIGN"
+    st.subheader("Result 2 (AlexNet): ")
+    st.write(result2)
